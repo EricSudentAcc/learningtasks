3c4448e386303d5996f98f3a638f639f5f039dfb
EricSudentAcc <100081443+EricSudentAcc@users.noreply.github.com>
2022-02-20 23:48:54 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>My Home Page</title>
+    </head>
+    <body>
+    <h1>Welcome to My Website! I'm Eric Deypalobos!</hl>
+    <ul>
+        <li><a href="index.php">HOME</a></li>
+        <li><a href="/LT2/index.html">LEARNING TASK 2</a></li>
+    </ul>
+    </body>
+</html
