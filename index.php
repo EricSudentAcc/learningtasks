94c2c69e3b79c400fc806e2640e586d51d7bf277
EricSudentAcc <100081443+EricSudentAcc@users.noreply.github.com>
2022-05-23 13:00:48 +0800

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <ul>
         <li><a href="index.php">HOME</a></li>
         <li><a href="/LT2/index.html">LEARNING TASK 2</a></li>
-        <li><a href="/LT3/Deypalobos_LT3.html">LEARNING TASK 3</a></li>
+        <li><a href="/LT3/index.html">LEARNING TASK 3</a></li>
         <li><a href="/CLE1MT/index.html">CLE1MT</a></li>
         <li><a href="/LT4/index.html">LT4</a></li>
         <li><a href="/LT5/index.html">LT5</a></li>
